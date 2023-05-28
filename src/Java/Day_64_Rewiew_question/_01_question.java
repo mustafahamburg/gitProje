3da216ba7737e315e9988dddb26fab1540df5607
mustafa <mstfozturk25hh@gmail.com>
2023-05-28 03:49:57 +0200

    question...

---
+++ src/Java/Day_64_Rewiew_question/_01_question.java
@@ -0,0 +1,50 @@
+package Java.Day_64_Rewiew_question;
+
+public class _01_question {
+    public static void main(String[] args) {
+      /* soru1: statik keyword nedir?(interwiew de kesinnnn)
+         Static Keyword 4 yerde var:
+         1-Variable  (public static int age)
+         2-Method    (main gibi)
+         3-Block     (Static {   })
+         4-Class     (optional)
+
+         Static Variable:
+         1-Class icerisinde her yerde erisilme
+         2-Degiskenin degeri en son verilen deger ile her yerde degismis olur.canli olarak ifade edilir
+         3-instance variable den class variable ye donusuyor
+         4-Class ismi ile diger class lardan cagirila imkani var ,hicbir obje create edilmaden cagirilabiliyor
+         5-hicbir obje create edilmaden hafizada obje olarak yer kapliyor
+
+         Static Method:
+         1-ClaSS ismi ile diger class lardan erisilme firsati verir
+         2-Main basta olmak uzere static olan method larda cagirilmasi saglanir
+            (tum methodlardan cagirilabilme imkani olusuyor)
+         3-Static methodlar override edilemezler
+
+
+         Static Block:
+         1-Her seyden once RUN olur,yani herseyden önce kosar,canli bir block diyebiliriz
+         2-Static degiskenleri,static block icerisinde direkt deger assigne edilebilir
+         3-Access modifiyr olmadan tanimlanir
+         4-inherit durumunda static block canli sekilde class a aktarilmis olur
+
+         soru 2: OOP nedir,prensipleri nelerdir?
+                 Obje nerede uretilir?
+
+         soru 3: interface ile abstrac class arasindaki farklar?
+
+         soru 4: interface ve abstrtac class a neden ihtiyac olur?
+         soru 5: Clean kod ahlaki? Solid kisaltmasi
+         soru 6: encaptulation nedir?
+         soru 7: Polymorphism nedir?
+
+
+
+
+
+
+
+       */
+    }
+}
